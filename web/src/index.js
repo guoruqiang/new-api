03f1e62ee4c12d0e58f5a1aabf4e79591f2b2921
GuoRuqiang <61670021+guoruqiang@users.noreply.github.com>
2024-09-19 06:42:13 +0000

    主页页边距

--- web/src/index.js
+++ web/src/index.js
@@ -34,7 +34,7 @@ root.render(
                 </Sider>
                 <Layout>
                   <Content
-                    style={{ overflowY: 'auto', padding: '24px' }}
+                    style={{ overflowY: 'auto', padding: '2px' }}
                   >
                     <App />
                   </Content>
